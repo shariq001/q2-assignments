7fb37cf1f47a108ada4d4a30b897176cd86e9429
Muhammad Shariq <muhammadshariqfazal@gmail.com>
2024-12-03 15:27:07 +0500

    update

--- 3-assignment/app/page.tsx
+++ 3-assignment/app/page.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='bg-gradient-to-r from-[#050A24] via-[#2D55FB] to-[#050A24] flex flex-col justify-center items-center p-[100px] h-screen'>
-      <a href="/Login" className='bg-black m-[30px] text-white py-[20px] px-[100px] rounded-[10px] duration-700 hover:px-[150px] hover:duration-700'>Login</a>
-      <a href="/Signup" className='bg-black m-[30px] text-white py-[20px] px-[95px] rounded-[10px] duration-700 hover:px-[150px] hover:duration-700'>Signup</a>
+    <div className='bg-gradient-to-b from-[#050A24] via-[#0B2F9F] to-[#050A24] flex justify-center items-center p-[100px] h-screen'>
+      <a href="/Login" className='bg-black m-[30px] text-white py-[20px] px-[100px] rounded-[50px] duration-700 hover:px-[150px] hover:duration-700 yuji'>Login</a>
+      <a href="/Signup" className='bg-black m-[30px] text-white py-[20px] px-[95px] rounded-[50px] duration-700 hover:px-[150px] hover:duration-700 yuji'>Signup</a>
     </div>
   )
 }
