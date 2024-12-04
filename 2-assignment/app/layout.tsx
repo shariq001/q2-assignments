a370d29b392a0592e49da615cbc8308c0e9ac0de
Muhammad Shariq <muhammadshariqfazal@gmail.com>
2024-12-04 17:13:39 +0500

    update

--- 2-assignment/app/layout.tsx
+++ 2-assignment/app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Governor Initiative</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       ><Header />
