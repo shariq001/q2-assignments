48d97378efb5b825561e3796efc01ea21417b303
Muhammad Shariq <muhammadshariqfazal@gmail.com>
2024-12-06 18:30:20 +0500

    update

--- 4-assignment/app/layout.tsx
+++ 4-assignment/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Header from "@/Components/Header";
-import Footer from "@/Components/Footer";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
